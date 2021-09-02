326370e282046ec8ec950cba5e15294593c6d803
Ira <irysukd111@gmail.com>
2021-09-02 18:09:54 +0300

    Version 3

--- MVC/controllers/PhoneBookController.js
+++ MVC/controllers/PhoneBookController.js
@@ -1,4 +1,4 @@
-let PhoneBookController = [];
+/*let PhoneBookController = [];
 PhoneBookController[0] = {
     id: 0,
     name: "Olexander Nykytin",
@@ -10,7 +10,7 @@ PhoneBookController[1] = {
     name: "Ivan Ivanov",
     email: "lol@ninydev.com",
     phone: "+380965747678"
-}
+}*/
 let PhoneBookAdv = [];
 PhoneBookAdv[0] = {
     id: 0,
@@ -30,27 +30,27 @@ exports.post = function (request, response){
     console.log("Run POST");
     if(!request.body) return response.sendStatus(400);
     if (request.body.name.length < 1) return response.sendStatus(400);
-    PhoneBookController.push(  {
+   /* PhoneBookController.push(  {
         id: lastId++,
         name: request.body.name,
         email: request.body.email ,
         phone: request.body.phone
     });
-    return response.sendStatus(206);
+    return response.sendStatus(206);*/
 
 }
 
 // read => GET
 exports.get = function (request, response){
     console.log("Run GET");
-    response.json(PhoneBookController);
+  // response.json(PhoneBookController);
     response.json(PhoneBookAdv);
 }
 
 // update => PUT
 exports.put = function (request, response){
     console.log("Run PUT");
-    for (let phone of PhoneBookController) {
+    /*for (let phone of PhoneBookController) {
         if (phone.id == request.body.id){
             phone.name = request.body.name;
             phone.email = request.body.email;
@@ -58,21 +58,22 @@ exports.put = function (request, response){
             break;
         }
 
-    }
-    response.json(PhoneBookController);
+    }*/
+    //response.json(PhoneBookController);
+    //response.json(PhoneBookAdv);
 }
 
 // delete => DELETE
 exports.delete = function (request, response){
     console.log("Run DELETE");
 
-    for (let i = 0; i < PhoneBookController.length; i++) {
+   /* for (let i = 0; i < PhoneBookController.length; i++) {
         if (PhoneBookController[i].id == request.body.id){
             PhoneBookController.splice(i,1);
             break;
         }
     }
-    response.json(PhoneBookController);
+    response.json(PhoneBookController);*/
 
 }
 
