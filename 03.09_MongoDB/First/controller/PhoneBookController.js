3cdddacb9efe9b0aa0225bebf6a86d3f36e2cf2b
Ira <irysukd111@gmail.com>
2021-09-03 21:10:16 +0300

    Version 8

---
+++ 03.09_MongoDB/First/controller/PhoneBookController.js
@@ -0,0 +1,121 @@
+/*let PhoneBookController = [];
+PhoneBookController[0] = {
+    id: 0,
+    name: "Olexander Nykytin",
+    email: "keeper@ninydev.com",
+    phone: "+380965747708"
+}
+PhoneBookController[1] = {
+    id: 1,
+    name: "Ivan Ivanov",
+    email: "lol@ninydev.com",
+    phone: "+380965747678"
+}*/
+let PhoneBookAdv = [];
+PhoneBookAdv[0] = {
+    id: 0,
+    name: "Olexander Nykytin",
+    email: ["keeper@ninydev.com", "nikitin_a@itstep.academy"],
+    phone: ["+380965747708","+380512192123"]
+}
+PhoneBookAdv[1] = {
+    id: 1,
+    name: "Ivan Ivanov",
+    email: ["lorix77602@mi166.com", "lori77602@mi17.com"],
+    phone: ["+380965742456","+380513332123"]
+}
+let lastId = 2;
+// create => POST
+exports.post = function (request, response){
+    console.log("Run POST");
+    if(!request.body) return response.sendStatus(400);
+    if (request.body.name.length < 1) return response.sendStatus(400);
+   /* PhoneBookController.push(  {
+        id: lastId++,
+        name: request.body.name,
+        email: request.body.email ,
+        phone: request.body.phone
+    });
+    return response.sendStatus(206);*/
+
+    PhoneBookAdv.push(  {
+        id: lastId++,
+        name: request.body.name,
+        email: request.body.email,
+        phone: request.body.phone
+    });
+    return response.sendStatus(206);
+
+}
+
+// read => GET
+exports.get = function (request, response){
+    console.log("Run GET");
+  // response.json(PhoneBookController);
+    response.json(PhoneBookAdv);
+}
+
+// update => PUT
+exports.put = function (request, response){
+    console.log("Run PUT");
+    /*for (let phone of PhoneBookController) {
+        if (phone.id == request.body.id){
+            phone.name = request.body.name;
+            phone.email = request.body.email;
+            phone.phone = request.body.phone;
+            break;
+        }
+
+    }*/
+    //response.json(PhoneBookController);
+
+    for (let phone of PhoneBookAdv) {
+        if (phone.id == request.body.id){
+            phone.name = request.body.name;
+           phone.email = request.body.email;
+           phone.phone = request.body.phone;
+            break;
+        }
+
+    }
+    response.json(PhoneBookAdv);
+}
+
+// delete => DELETE
+exports.delete = function (request, response){
+    console.log("Run DELETE");
+
+   /* for (let i = 0; i < PhoneBookController.length; i++) {
+        if (PhoneBookController[i].id == request.body.id){
+            PhoneBookController.splice(i,1);
+            break;
+        }
+    }
+    response.json(PhoneBookController);*/
+
+    const index = PhoneBookAdv.findIndex(p => p.id == request.body.id );
+    if(index !== -1){
+        PhoneBookAdv.splice(index, 1);
+    }
+
+    response.json(PhoneBookAdv);
+
+}
+
+
+
+
+
+
+
+
+
+/*
+
+let PhoneBookAdvPlus = [];
+PhoneBookAdvPlus[0] = {
+    id: 0,
+    name: "Olexander Nykytin",
+    email: [{name :" Личный", mail: "keeper@ninydev.com"},{name :" рабочий", mail: "nikitin_a@itstep.academy"}],
+    phone: [{name :" Личный", mail: "+380965747708"},{name :" соседка", mail: "+380512192123"}]
+}*/
--- 03.09_MongoDB/First/httpMongo.js
+++ 03.09_MongoDB/First/httpMongo.js
@@ -3,11 +3,10 @@ const express = require('express');
 const MongoClient = require("mongodb").MongoClient;
 //const objectId = require("mongodb").ObjectId;
 
-const uri = "mongodb+srv://Iryna:PzFeiLEU4triOUbF@cluster0.l28g9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const mongoClient = new MongoClient(uri);
+const uri = "mongodb+srv://Iryna:PzFeiLEU4triOUbF@cluster0.l28g9.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());
@@ -23,7 +22,7 @@ app.route ("/api/students")
     .put(StudentsController.put)
     .delete(StudentsController.delete);*/
 
-const PhoneBookController = require("./controllers/PhoneBookController");
+const PhoneBookController = require("./controller/PhoneBookController");
 
 // маршрутизатор
 app.route ("/api/phoneBook")
@@ -33,4 +32,11 @@ app.route ("/api/phoneBook")
     .delete(PhoneBookController.delete);
 
 
-app.listen(3000);
+//app.listen(3000);
+
+
+mongoClient.connect(function (err){
+    app.listen(3000, function (){
+        console.log("http://localhost:3000");
+    })
+});
